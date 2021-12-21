2557f9a6f2cd938f1331fdfd19f95906cc57a514
danilooliveira <danilooliveira@pinterest.com>
2021-12-21 15:29:16 -0800

    added file create

--- .github/actions/deploy_test.sh
+++ .github/actions/deploy_test.sh
@@ -4,6 +4,7 @@ set -ev
 mkdir /tmp/blt-deploy
 cd /tmp/blt-deploy
 git init
+touch /tmp/blt-deploy/.git/test.txt
 cd /
 if [ -d "/tmp/blt-deploy" ]; then rm -rf "/tmp/blt-deploy"; fi
 #$BLT_DIR/bin/blt artifact:deploy --commit-msg "Automated commit by Github Actions for Build $GITHUB_RUN_NUMBER of workflow $GITHUB_WORKFLOW" --branch "$GITHUB_REF_NAME-build" --no-interaction --verbose --site=${SITE}
