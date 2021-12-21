6c7036dce12a7d4f0e0abcc3eb32fd40e951b967
danilooliveira <danilooliveira@pinterest.com>
2021-12-21 15:40:51 -0800

    rm -r only

--- .github/actions/deploy_test.sh
+++ .github/actions/deploy_test.sh
@@ -6,7 +6,7 @@ cd /tmp/blt-deploy
 git init
 touch /tmp/blt-deploy/.git/test.txt
 cd /
-if [ -d "/tmp/blt-deploy" ]; then rm -rf "/tmp/blt-deploy"; fi
+if [ -d "/tmp/blt-deploy" ]; then rm -r "/tmp/blt-deploy"; fi
 #$BLT_DIR/bin/blt artifact:deploy --commit-msg "Automated commit by Github Actions for Build $GITHUB_RUN_NUMBER of workflow $GITHUB_WORKFLOW" --branch "$GITHUB_REF_NAME-build" --no-interaction --verbose --site=${SITE}
 
 set +v
