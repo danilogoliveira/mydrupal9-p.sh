cb5061c0c8b6e4daa75a89c374d6c4f649086c48
danilooliveira <danilooliveira@pinterest.com>
2021-12-21 17:36:31 -0800

    fixed added mv

--- .github/actions/deploy_test.sh
+++ .github/actions/deploy_test.sh
@@ -6,7 +6,7 @@ cd /tmp/blt-deploy
 git init
 touch /tmp/blt-deploy/.git/test.txt
 cd /
-if [ -d "/tmp/blt-deploy/.git" ]; then mv -f "/tmp/blt-deploy/.git /tmp/"; fi
+if [ -d "/tmp/blt-deploy/.git" ]; then mv -f "/tmp/blt-deploy/.git" "/tmp/"; fi
 if [ -d "/tmp/blt-deploy" ]; then rm -r "/tmp/blt-deploy"; fi
 #$BLT_DIR/bin/blt artifact:deploy --commit-msg "Automated commit by Github Actions for Build $GITHUB_RUN_NUMBER of workflow $GITHUB_WORKFLOW" --branch "$GITHUB_REF_NAME-build" --no-interaction --verbose --site=${SITE}
 echo "Vamos q Vamos"
